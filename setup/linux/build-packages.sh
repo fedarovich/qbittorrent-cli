34b0efe6c234948e0a298725de0c863f1a2e2c31
Pavel Fedarovich <p.fedarovich@gmail.com>
2018-04-20 14:06:21 +0300

    Updated build script.

--- setup/linux/build-packages.sh
+++ setup/linux/build-packages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+VERSION="${BUILD_BUILDNUMBER%.*}"
+ITERATION="${BUILD_BUILDNUMBER##*.}"
+
 ###############################################################################
 # Create packages for x64                                                     #
 ###############################################################################
@@ -12,7 +15,7 @@ tar -xvf "$BUILD_BINARIESDIRECTORY/tgz/qbt-linux-x64-*.tag.gz" -C "$PkgRoot/usr/
 ln -sf "$PkgRoot/usr/lib/qbittorrent-cli/qbt" "$PkgRoot/usr/bin/qbt"
 
 #build packages
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial/x64" && fpm -s dir -t deb -f -C "$PkgRoot" --name qbittorrent-cli --version $BUILD_BUILDNUMBER --iteration 1 -a amd64 --description "qBittorrent remote command line client." -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu55
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial" && fpm -s dir -t deb -f -C "$PkgRoot" --name qbittorrent-cli --version $VERSION --iteration $ITERATION -a amd64 --description "qBittorrent remote command line client." -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu55
 
 ###############################################################################
 # Create packages for ARM                                                     #
@@ -24,7 +27,7 @@ tar -xvf "$BUILD_BINARIESDIRECTORY/tgz/qbt-linux-arm-*.tag.gz" -C "$PkgRoot/usr/
 ln -sf "$PkgRoot/usr/lib/qbittorrent-cli/qbt" "$PkgRoot/usr/bin/qbt"
 
 #build packages
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial/armhf" && fpm -s dir -t deb -f -C "$PkgRoot" --name qbittorrent-cli --version $BUILD_BUILDNUMBER --iteration 1 -a armhf --description "qBittorrent remote command line client." -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu55
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial" && fpm -s dir -t deb -f -C "$PkgRoot" --name qbittorrent-cli --version $VERSION --iteration $ITERATION -a armhf --description "qBittorrent remote command line client." -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu55
 
 ###############################################################################
 # Clean up                                                     #
