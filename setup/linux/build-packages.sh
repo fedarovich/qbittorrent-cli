4a59e2795cd7b66b7a4fb7139dead67c24e88f29
Pavel Fedarovich <p.fedarovich@gmail.com>
2018-04-23 16:15:28 +0300

    Added more metadata to packages.

--- setup/linux/build-packages.sh
+++ setup/linux/build-packages.sh
@@ -4,6 +4,10 @@ PkgName="qbittorrent-cli"
 PkgDesc="qBittorrent remote command line client."
 PkgVersion="${BUILD_BUILDNUMBER%.*}"
 PkgIteration="${BUILD_BUILDNUMBER##*.}"
+PkgLicense="MIT"
+PkgVendor="Pavel Fedarovich"
+PkgMaintainer="Pavel Fedarovich"
+PkgHomepage="https://github.com/fedarovich/qbittorrent-cli"
 
 ###############################################################################
 # Create packages for x64                                                     #
@@ -18,15 +22,15 @@ chmod +x "$PkgRoot/usr/lib/qbittorrent-cli/qbt"
 ln -sfr "$PkgRoot/usr/lib/qbittorrent-cli/qbt" "$PkgRoot/usr/bin/qbt"
 
 #build packages
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/trusty" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a amd64 --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu52
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a amd64 --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu55
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/artful" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a amd64 --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d "libssl1.0.0 | libssl1.0.2" -d libicu57
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/bionic" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a amd64 --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu60
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/trusty" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a amd64 --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu52
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a amd64 --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu55
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/artful" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a amd64 --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d "libssl1.0.0 | libssl1.0.2" -d libicu57
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/bionic" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a amd64 --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu60
 
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/fedora/25" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a x86_64 --description "$PkgDesc" -p "$_" -d libunwind -d libcurl -d openssl-libs -d libicu
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/fedora/26" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a x86_64 --description "$PkgDesc" -p "$_" -d libunwind -d libcurl -d openssl-libs -d libicu -d compat-openssl10
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/fedora/25" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a x86_64 --description "$PkgDesc" -p "$_" -d libunwind -d libcurl -d openssl-libs -d libicu
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/fedora/26" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a x86_64 --description "$PkgDesc" -p "$_" -d libunwind -d libcurl -d openssl-libs -d libicu -d compat-openssl10
 
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/opensuse/42" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a x86_64 --description "$PkgDesc" -p "$_" -d libunwind -d libcurl4 -d libssl43 -d libicu
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/opensuse/42" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a x86_64 --description "$PkgDesc" -p "$_" -d libunwind -d libcurl4 -d libssl43 -d libicu
 
 ###############################################################################
 # Create packages for ARM                                                     #
@@ -39,15 +43,15 @@ chmod +x "$PkgRoot/usr/lib/qbittorrent-cli/qbt"
 ln -sfr "$PkgRoot/usr/lib/qbittorrent-cli/qbt" "$PkgRoot/usr/bin/qbt"
 
 #build packages
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/trusty" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a armhf --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu52
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a armhf --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu55
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/artful" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a armhf --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d "libssl1.0.0 | libssl1.0.2" -d libicu57
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/bionic" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a armhf --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu60
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/trusty" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a armhf --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu52
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a armhf --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu55
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/artful" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a armhf --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d "libssl1.0.0 | libssl1.0.2" -d libicu57
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/bionic" && fpm -s dir -t deb -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a armhf --description "$PkgDesc" -p "$_" -d libunwind8 -d libcurl3 -d libssl1.0.0 -d libicu60
 
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/fedora/25" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a armv7hl --description "$PkgDesc" -p "$_" -d libunwind -d libcurl -d openssl-libs -d libicu
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/fedora/26" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a armv7hl --description "$PkgDesc" -p "$_" -d libunwind -d libcurl -d openssl-libs -d libicu -d compat-openssl10
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/fedora/25" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a armv7hl --description "$PkgDesc" -p "$_" -d libunwind -d libcurl -d openssl-libs -d libicu
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/fedora/26" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a armv7hl --description "$PkgDesc" -p "$_" -d libunwind -d libcurl -d openssl-libs -d libicu -d compat-openssl10
 
-mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/opensuse/42" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration -a armv7hl --description "$PkgDesc" -p "$_" -d libunwind -d libcurl4 -d libssl43 -d libicu
+mkdir -p "$BUILD_BINARIESDIRECTORY/pkg/opensuse/42" && fpm -s dir -t rpm -f -C "$PkgRoot" --name $PkgName --version $PkgVersion --iteration $PkgIteration --license "$PkgLicense" --vendor "$PkgVendor" --maintainer "$PkgMaintainer" --url "$PkgHomepage" -a armv7hl --description "$PkgDesc" -p "$_" -d libunwind -d libcurl4 -d libssl43 -d libicu
 
 ###############################################################################
 # Clean up                                                     #
