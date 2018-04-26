bee360eeab3b243da52c0643349ec28b79f0c915
Pavel Fedarovich <p.fedarovich@gmail.com>
2018-04-27 01:00:34 +0300

    Added uninstall script for the Mac OS installer.

--- setup/mac/build-packages.sh
+++ setup/mac/build-packages.sh
@@ -7,6 +7,8 @@ mkdir -p "$PkgRoot/usr/local/qbittorrent-cli"
 tar -xvf "$BUILD_BINARIESDIRECTORY/tgz/cli/qbt-osx-x64-$BUILD_BUILDNUMBER.tar.gz" -C "$PkgRoot/usr/local/qbittorrent-cli"
 chmod +x "$PkgRoot/usr/local/qbittorrent-cli/qbt"
 ln -sf "../qbittorrent-cli/qbt" "$PkgRoot/usr/local/bin/qbt"
+cp -f "$BUILD_SOURCESDIRECTORY/setup/mac/scripts/uninstall.sh" "$PkgRoot/usr/local/qbittorrent-cli/uninstall"
+chmod +x "$PkgRoot/usr/local/qbittorrent-cli/uninstall"
 
 #build packages
 mkdir -p "$BUILD_BINARIESDIRECTORY/pkg" && pkgbuild --root "$PkgRoot" --identifier "com.fedarovich.pkg.qbittorrent-cli" --install-location / --version $BUILD_BUILDNUMBER "$_/qbittorrent-cli-${BUILD_BUILDNUMBER}.pkg"
---
+++ setup/mac/scripts/uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]
+then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+read -p "Are you sure that you want to uninstall qBittorrent CLI? (y/N) " -n 1 -r
+echo
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  rm -fv  /usr/local/bin/qbt
+  rm -fvr /usr/local/qbittorrent-cli
+  pkgutil --forget com.fedarovich.pkg.qbittorrent-cli
+fi
+	
