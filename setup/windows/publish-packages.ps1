1e867f2a14f09db7c184ef2c2c6c682d19878696
Pavel Fedarovich <p.fedarovich@gmail.com>
2018-05-08 15:05:55 +0300

    Running curl in silent mode.

--- setup/windows/publish-packages.ps1
+++ setup/windows/publish-packages.ps1
@@ -6,5 +6,5 @@ Param(
 
 Remove-Item Alias:Curl
 
-curl -T "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\publish\qbittorrent-cli-$env:BUILD_BUILDNUMBER-x86.msi" "-ufedarovich:$BinTrayApiKey" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-windows/qbittorrent-cli/$env:BUILD_BUILDNUMBER/qbittorrent-cli-$env:BUILD_BUILDNUMBER-x86.msi?publish=1"
-curl -T "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\publish\qbittorrent-cli-$env:BUILD_BUILDNUMBER-x64.msi" "-ufedarovich:$BinTrayApiKey" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-windows/qbittorrent-cli/$env:BUILD_BUILDNUMBER/qbittorrent-cli-$env:BUILD_BUILDNUMBER-x64.msi?publish=1"
+curl --silent --show-error --upload-file "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\publish\qbittorrent-cli-$env:BUILD_BUILDNUMBER-x86.msi" "-ufedarovich:$BinTrayApiKey" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-windows/qbittorrent-cli/$env:BUILD_BUILDNUMBER/qbittorrent-cli-$env:BUILD_BUILDNUMBER-x86.msi?publish=1"
+curl --silent --show-error --upload-file "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\publish\qbittorrent-cli-$env:BUILD_BUILDNUMBER-x64.msi" "-ufedarovich:$BinTrayApiKey" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-windows/qbittorrent-cli/$env:BUILD_BUILDNUMBER/qbittorrent-cli-$env:BUILD_BUILDNUMBER-x64.msi?publish=1"
