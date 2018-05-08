1a57c73afb59f3eb191b61e97ef2eb5c129c436c
Pavel Fedarovich <p.fedarovich@gmail.com>
2018-05-08 13:54:53 +0300

    Fixed publish-packages.ps1 script

--- setup/windows/publish-packages.ps1
+++ setup/windows/publish-packages.ps1
@@ -1,4 +1,4 @@
 Remove-Item Alias:Curl
 
-curl -T "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\cli\qbittorrent-cli-$env:Version-x86" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-windows/qbittorrent-cli/$env:Version/qbittorrent-cli-$env:Version-x86.msi?publish=1"
-curl -T "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\cli\qbittorrent-cli-$env:Version-x64" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-windows/qbittorrent-cli/$env:Version/qbittorrent-cli-$env:Version-x64.msi?publish=1"
+curl -T "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\cli\qbittorrent-cli-$env:BUILD_BUILDNUMBER-x86" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-windows/qbittorrent-cli/$env:BUILD_BUILDNUMBER/qbittorrent-cli-$env:BUILD_BUILDNUMBER-x86.msi?publish=1"
+curl -T "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\cli\qbittorrent-cli-$env:BUILD_BUILDNUMBER-x64" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-windows/qbittorrent-cli/$env:BUILD_BUILDNUMBER/qbittorrent-cli-$env:BUILD_BUILDNUMBER-x64.msi?publish=1"
