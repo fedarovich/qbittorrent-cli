0a486de4e0d4eabf5d3e14d7380f881059925c5f
Pavel Fedarovich <p.fedarovich@gmail.com>
2018-05-10 13:39:53 +0300

    Script for building chocolatey package

--- setup/windows/build-packages.ps1
+++ setup/windows/build-packages.ps1
@@ -14,13 +14,21 @@ choco new qbittorrent-cli `
     maintainerrepo="https://github.com/fedarovich/qbittorrent-cli" `
     installertype=msi `
     url="https://dl.bintray.com/fedarovich/qbittorrent-cli-windows/qbittorrent-cli-$env:BUILD_BUILDNUMBER-x86.msi" `
-    url64="https://dl.bintray.com/fedarovich/qbittorrent-cli-windows/qbittorrent-cli-$env:BUILD_BUILDNUMBER-x64.msi"
+    url64="https://dl.bintray.com/fedarovich/qbittorrent-cli-windows/qbittorrent-cli-$env:BUILD_BUILDNUMBER-x64.msi" `
+    checksum="$Hash32.Hash" `
+    checksum64="$Hash64.Hash"
 
-#TODO: Add LICENSE.TXT
-#TODO: Add VERIFICATION.txt
+pushd qbittorrent-cli
+
+Invoke-WebRequest https://rawgit.com/fedarovich/qbittorrent-cli/master/LICENSE -OutFile .\tools\LICENSE.txt
+
+cp -Path "$env:BUILD_SOURCESDIRECTORY\setup\windows\VERIFICATION.txt" -Destination .\tools\VERIFICATION.txt -Force
 
 # Remove comments from the chocolateyinstall.ps1 script
 $f="$env:BUILD_BINARIESDIRECTORY\qbittorrent-cli\tools\chocolateyinstall.ps1"
 gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
 
+choco pack --outputdirectory "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\publish"
+
+popd
 popd
