0e6ee89fc5d82d5ac4a11d1a9cda54dc70962f1d
Pavel Fedarovich <p.fedarovich@gmail.com>
2019-02-19 18:12:14 +0300

    Added "torrent share" command (incomplete!)

---
+++ src/QBittorrent.CommandLineInterface/Commands/TorrentCommand.Share.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using NJsonSchema.Infrastructure;
+using QBittorrent.Client;
+using QBittorrent.CommandLineInterface.ColorSchemes;
+
+namespace QBittorrent.CommandLineInterface.Commands
+{
+    [Subcommand(typeof(Share))]
+    public partial class TorrentCommand
+    {
+        [Command("share", "sharing", Description = "Manages torrent sharing limits.")]
+        public class Share : TorrentSpecificCommandBase
+        {
+            [Option("-r|--ratio <VALUE>", "Set the ratio limit (number|GLOBAL|NONE)", CommandOptionType.SingleValue)]
+            public double? RatioLimit { get; set; }
+
+            [Option("-s|--seeding-time <VALUE>", "Set the share limit ([d.]HH:mm[:ss]|GLOBAL|NONE)", CommandOptionType.SingleValue)]
+            public TimeSpan? SeedingTimeLimit { get; set; }
+
+            protected override async Task<int> OnExecuteTorrentSpecificAsync(QBittorrentClient client, CommandLineApplication app, IConsole console)
+            {
+                var partialData = await client.GetPartialDataAsync();
+                if (!partialData.TorrentsChanged.TryGetValue(Hash, out var info))
+                {
+                    console.WriteLineColored($"No torrent matching hash {Hash} is found.", ColorScheme.Current.Warning);
+                    return ExitCodes.NotFound;
+                }
+
+                if (RatioLimit != null || SeedingTimeLimit != null)
+                {
+                    await client.SetShareLimitsAsync(Hash,
+                        RatioLimit ?? info.RatioLimit ?? ShareLimits.Ratio.Global,
+                        SeedingTimeLimit ?? info.SeedingTimeLimit ?? ShareLimits.SeedingTime.Global);
+                    return ExitCodes.Success;
+                }
+
+                // TODO: Print current state.
+
+                return ExitCodes.Success;
+            }
+        }
+    }
+}
---
+++ src/QBittorrent.CommandLineInterface/ViewModels/TorrentShareViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using BencodeNET.Torrents;
+using QBittorrent.Client;
+
+namespace QBittorrent.CommandLineInterface.ViewModels
+{
+    public readonly struct TorrentShareViewModel
+    {
+        private readonly TorrentPartialInfo _partialInfo;
+        private readonly TorrentProperties _properties;
+
+        public TorrentShareViewModel(TorrentProperties properties, TorrentPartialInfo wrappedObject)
+        {
+            _properties = properties ?? throw new ArgumentNullException(nameof(properties));
+            _partialInfo = wrappedObject ?? throw new ArgumentNullException(nameof(wrappedObject));
+        }
+
+        [Display(Name = "Ratio")]
+        public double Ratio => _properties.ShareRatio;
+
+        [Display(Name = "Ratio limit")]
+        [DisplayFormat(NullDisplayText = "n/a")]
+        public double? RatioLimit => _partialInfo.RatioLimit;
+
+        [Display(Name = "Seeding time")]
+        [DisplayFormat(NullDisplayText = "n/a")]
+        public TimeSpan? SeedingTime => _properties.SeedingTime;
+
+        [Display(Name = "Seeding time limit")]
+        [DisplayFormat(NullDisplayText = "n/a")]
+        public TimeSpan? SeedingTimeLimit => _partialInfo.SeedingTimeLimit;
+    }
+}
