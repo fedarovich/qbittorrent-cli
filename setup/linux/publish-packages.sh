c4a39b299c7e5d8c57cdcc7dbb5f10ab1364f6d7
Pavel Fedarovich <p.fedarovich@gmail.com>
2021-04-22 18:15:51 +0300

    Updated build/publish scripts to use packagecloud.

--- setup/linux/publish-packages.sh
+++ setup/linux/publish-packages.sh
@@ -11,48 +11,34 @@ PkgFullVersion="$PkgVersion-$PkgIteration"
 ###############################################################################
 
 # amd64
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial/qbittorrent-cli_${PkgFullVersion}_amd64.deb" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/xenial/pool/main/q/qbittorrent-cli_${PkgFullVersion}_amd64.deb;deb_distribution=xenial;deb_component=main;deb_architecture=amd64?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/artful/qbittorrent-cli_${PkgFullVersion}_amd64.deb" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/artful/pool/main/q/qbittorrent-cli_${PkgFullVersion}_amd64.deb;deb_distribution=artful;deb_distribution=stretch;deb_component=main;deb_architecture=amd64?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/bionic/qbittorrent-cli_${PkgFullVersion}_amd64.deb" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/bionic/pool/main/q/qbittorrent-cli_${PkgFullVersion}_amd64.deb;deb_distribution=bionic;deb_component=main;deb_architecture=amd64?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/disco/qbittorrent-cli_${PkgFullVersion}_amd64.deb"  "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/disco/pool/main/q/qbittorrent-cli_${PkgFullVersion}_amd64.deb;deb_distribution=disco;deb_distribution=buster;deb_component=main;deb_architecture=amd64?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/focal/qbittorrent-cli_${PkgFullVersion}_amd64.deb"  "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/focal/pool/main/q/qbittorrent-cli_${PkgFullVersion}_amd64.deb;deb_distribution=focal;deb_distribution=groovy;deb_component=main;deb_architecture=amd64?publish=1"
+package_cloud push fedarovich/qbittorrent-cli/debian/stretch "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/universal/qbittorrent-cli_${PkgFullVersion}_amd64.deb"
+package_cloud push fedarovich/qbittorrent-cli/ubuntu/xenial "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/universal/qbittorrent-cli_${PkgFullVersion}_amd64.deb"
 
 # armhf
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial/qbittorrent-cli_${PkgFullVersion}_armhf.deb" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/xenial/pool/main/q/qbittorrent-cli_${PkgFullVersion}_armhf.deb;deb_distribution=xenial;deb_component=main;deb_architecture=armhf?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/artful/qbittorrent-cli_${PkgFullVersion}_armhf.deb" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/artful/pool/main/q/qbittorrent-cli_${PkgFullVersion}_armhf.deb;deb_distribution=artful;deb_distribution=stretch;deb_component=main;deb_architecture=armhf?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/bionic/qbittorrent-cli_${PkgFullVersion}_armhf.deb" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/bionic/pool/main/q/qbittorrent-cli_${PkgFullVersion}_armhf.deb;deb_distribution=bionic;deb_component=main;deb_architecture=armhf?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/disco/qbittorrent-cli_${PkgFullVersion}_armhf.deb"  "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/disco/pool/main/q/qbittorrent-cli_${PkgFullVersion}_armhf.deb;deb_distribution=disco;deb_distribution=buster;deb_component=main;deb_architecture=armhf?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/focal/qbittorrent-cli_${PkgFullVersion}_armhf.deb"  "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/focal/pool/main/q/qbittorrent-cli_${PkgFullVersion}_armhf.deb;deb_distribution=focal;deb_distribution=groovy;deb_component=main;deb_architecture=armhf?publish=1"
+package_cloud push fedarovich/qbittorrent-cli/debian/stretch "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/universal/qbittorrent-cli_${PkgFullVersion}_armhf.deb"
+package_cloud push fedarovich/qbittorrent-cli/ubuntu/xenial "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/universal/qbittorrent-cli_${PkgFullVersion}_armhf.deb"
 
 # arm64
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/xenial/qbittorrent-cli_${PkgFullVersion}_arm64.deb" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/xenial/pool/main/q/qbittorrent-cli_${PkgFullVersion}_arm64.deb;deb_distribution=xenial;deb_component=main;deb_architecture=arm64?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/artful/qbittorrent-cli_${PkgFullVersion}_arm64.deb" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/artful/pool/main/q/qbittorrent-cli_${PkgFullVersion}_arm64.deb;deb_distribution=artful;deb_distribution=stretch;deb_component=main;deb_architecture=arm64?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/bionic/qbittorrent-cli_${PkgFullVersion}_arm64.deb" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/bionic/pool/main/q/qbittorrent-cli_${PkgFullVersion}_arm64.deb;deb_distribution=bionic;deb_component=main;deb_architecture=arm64?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/disco/qbittorrent-cli_${PkgFullVersion}_arm64.deb"  "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/disco/pool/main/q/qbittorrent-cli_${PkgFullVersion}_arm64.deb;deb_distribution=disco;deb_distribution=buster;deb_component=main;deb_architecture=arm64?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/focal/qbittorrent-cli_${PkgFullVersion}_arm64.deb"  "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-debian/qbittorrent-cli/${PkgFullVersion}/focal/pool/main/q/qbittorrent-cli_${PkgFullVersion}_arm64.deb;deb_distribution=focal;deb_distribution=groovy;deb_component=main;deb_architecture=arm64?publish=1"
-
-curl -X POST --user "fedarovich:$1" https://api.bintray.com/calc_metadata/fedarovich/qbittorrent-cli-debian
+package_cloud push fedarovich/qbittorrent-cli/debian/stretch "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/universal/qbittorrent-cli_${PkgFullVersion}_arm64.deb"
+package_cloud push fedarovich/qbittorrent-cli/ubuntu/xenial "$BUILD_BINARIESDIRECTORY/pkg/ubuntu/universal/qbittorrent-cli_${PkgFullVersion}_arm64.deb"
 
 ###############################################################################
 # Upload Fedora/CentOS/RHEL packages                                          #
 ###############################################################################
 
 # x86_64
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/centos/7/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm"  "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/centos/7/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/fedora/26/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/fedora/26/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/opensuse/15/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/opensuse/15/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/opensuse/tw/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/opensuse/tw/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm?publish=1"
+package_cloud push fedarovich/qbittorrent-cli/fedora/32 "$BUILD_BINARIESDIRECTORY/pkg/fedora/32/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm"
+package_cloud push fedarovich/qbittorrent-cli/opensuse/15.0 "$BUILD_BINARIESDIRECTORY/pkg/opensuse/15/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm"
+package_cloud push fedarovich/qbittorrent-cli/opensuse/tw "$BUILD_BINARIESDIRECTORY/pkg/opensuse/tw/qbittorrent-cli-${PkgFullVersion}.x86_64.rpm"
 
 # armv7hl
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/centos/7/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm"  "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/centos/7/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/fedora/26/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/fedora/26/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/opensuse/15/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/opensuse/15/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/opensuse/tw/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/opensuse/tw/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm?publish=1"
+package_cloud push fedarovich/qbittorrent-cli/fedora/32 "$BUILD_BINARIESDIRECTORY/pkg/fedora/32/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm"
+package_cloud push fedarovich/qbittorrent-cli/opensuse/15.0 "$BUILD_BINARIESDIRECTORY/pkg/opensuse/15/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm"
+package_cloud push fedarovich/qbittorrent-cli/opensuse/tw "$BUILD_BINARIESDIRECTORY/pkg/opensuse/tw/qbittorrent-cli-${PkgFullVersion}.armv7hl.rpm"
 
 # aarch64
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/centos/7/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm"  "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/centos/7/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/fedora/26/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/fedora/26/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/opensuse/15/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/opensuse/15/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm?publish=1"
-curl -T "$BUILD_BINARIESDIRECTORY/pkg/opensuse/tw/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm" "-ufedarovich:$1" "https://api.bintray.com/content/fedarovich/qbittorrent-cli-rpm/qbittorrent-cli/${PkgFullVersion}/opensuse/tw/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm?publish=1"
+package_cloud push fedarovich/qbittorrent-cli/fedora/32 "$BUILD_BINARIESDIRECTORY/pkg/fedora/32/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm"
+package_cloud push fedarovich/qbittorrent-cli/opensuse/15.0 "$BUILD_BINARIESDIRECTORY/pkg/opensuse/15/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm"
+package_cloud push fedarovich/qbittorrent-cli/opensuse/tw "$BUILD_BINARIESDIRECTORY/pkg/opensuse/tw/qbittorrent-cli-${PkgFullVersion}.aarch64.rpm"
 
 set +x
